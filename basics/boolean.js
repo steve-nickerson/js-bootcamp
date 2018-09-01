6e6d27c6c32b6d5ef9c4f65cb6ddc91a87d5f10d
steve <s97095519@hotmail.com>
2018-08-31 21:07:38 -0700

    initial commit

---
+++ basics/boolean.js
@@ -0,0 +1,6 @@
+let temp = 31
+
+let isFreezing = temp === 31
+
+console.log(isFreezing) // eslint-disable-line 
+
---
+++ basics/temp-convert.js
@@ -0,0 +1,11 @@
+let farenheit = 32 // 0c -> 273.15K
+
+// calculate celsius and store in celsius variable
+// print that value
+let celsius = (farenheit - 32) * 5 / 9
+console.log(celsius) // eslint-disable-line no-console
+
+// calculate kelvin value and store in variable
+// print that value
+let kelvin = (farenheit + 459.67) * 5 / 9
+console.log(kelvin) // eslint-disable-line no-console
